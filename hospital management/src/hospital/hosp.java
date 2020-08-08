4873f2667a7c081d93d534a72db2ff8e5f7cc4af
charithcshetty <64842641+charithcshetty@users.noreply.github.com>
2020-08-08 09:31:37 +0530

    Add files via upload

---
+++ hospital management/src/hospital/hosp.java
@@ -0,0 +1,992 @@
+package hospital;
+
+
+
+import java.io.*;
+
+import java.util.*;
+
+import java.util.Calendar;
+
+
+
+class staff
+
+{
+
+ String sid, sname, desg, sex;
+
+ int salary;
+
+ void new_staff()
+
+ {
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("id:-");
+  sid = input.nextLine();
+
+  System.out.print("name:-");
+  sname = input.nextLine();
+
+  System.out.print("desigination:-");
+  desg = input.nextLine();
+
+  System.out.print("sex:-");
+  sex = input.nextLine();
+
+  System.out.print("salary:-");
+  salary = input.nextInt();
+
+ }
+
+ void staff_info()
+
+ {
+
+  System.out.println(sid + "\t" + sname + "\t" + sex + "\t" + salary);
+
+ }
+
+}
+
+
+
+class doctor
+
+{
+
+ String did, dname, specilist, appoint, doc_qual;
+
+ int droom;
+
+
+
+ void new_doctor()
+
+ {
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("id:-");
+  did = input.nextLine();
+
+  System.out.print("name:-");
+  dname = input.nextLine();
+
+  System.out.print("specialization:-");
+  specilist = input.nextLine();
+
+  System.out.print("work time:-");
+  appoint = input.nextLine();
+
+  System.out.print("qualification:-");
+  doc_qual = input.nextLine();
+
+  System.out.print("room no.:-");
+  droom = input.nextInt();
+
+ }
+
+ void doctor_info()
+
+ {
+
+  System.out.println(did + "\t" + dname + "  \t" + specilist + "     \t" + appoint + "    \t" + doc_qual + "       \t" + droom);
+
+
+ }
+
+}
+
+
+
+class patient
+
+{
+
+ String pid, pname, disease, sex, admit_status;
+
+ int age;
+
+
+
+ void new_patient()
+
+ {
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("id:-");
+  pid = input.nextLine();
+
+  System.out.print("name:-");
+  pname = input.nextLine();
+
+  System.out.print("disease:-");
+  disease = input.nextLine();
+
+  System.out.print("sex:-");
+  sex = input.nextLine();
+
+  System.out.print("admit_status:-");
+  admit_status = input.nextLine();
+
+  System.out.print("age:-");
+  age = input.nextInt();
+
+ }
+
+ void patient_info()
+
+ {
+
+  System.out.println(pid + "\t" + pname + " \t" + disease + "     \t" + sex + "      \t" + admit_status + "\t" + age);
+
+ }
+ void genbill()
+ 
+ {int n;Double cha,hf,total,cb;String name;
+	 Scanner input=new Scanner(System.in);
+	 	System.out.println("------------------------------------------------------------------ \n "+"\t\t\t\tCITY HOSPITAL\n"+"------------------------------------------------------------------ "+"\t\t\t\t\t\n HOSPITAL BILL\n");
+	 	System.out.println("Enter the name of the patient\n");
+	 	name=input.nextLine();
+	 	System.out.println("Enter the no of days admitted \n");
+	 	n=input.nextInt();
+	 	System.out.println("Enter the cost of bed in ward per day");
+	 	cb=input.nextDouble();
+	 	
+	 	System.out.println("Enter the charge of treatment \n");
+	 	cha=input.nextDouble();
+	 	System.out.println("Enter the consultation fee \n");
+	 	hf=input.nextDouble();
+	 	total=(n*cb)+cha+hf;
+	 	System.out.println("The bill generated for the patient "+name+" is ");
+	 	System.out.println(total+" Rupees\n");
+	 	
+	 	
+	 	
+ }
+
+
+}
+
+
+
+class medical
+
+{
+
+ String med_name, med_comp, exp_date;
+
+ int med_cost, count;
+
+
+ void new_medi()
+
+ {
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("name:-");
+  med_name = input.nextLine();
+
+  System.out.print("comp:-");
+  med_comp = input.nextLine();
+
+  System.out.print("exp_date:-");
+  exp_date = input.nextLine();
+
+  System.out.print("cost:-");
+  med_cost = input.nextInt();
+
+  System.out.print("no of unit:-");
+  count = input.nextInt();
+
+ }
+
+ void find_medi()
+
+ {
+
+  System.out.println(med_name + "  \t" + med_comp + "    \t" + exp_date + "     \t" + med_cost);
+
+ }
+
+}
+
+
+class lab
+
+{
+
+ String fecility;
+
+ int lab_cost;
+
+
+ void new_feci()
+
+ {
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("facility:-");
+  fecility = input.nextLine();
+
+  System.out.print("cost:-");
+  lab_cost = input.nextInt();
+
+ }
+
+ void feci_list()
+
+ {
+
+  System.out.println(fecility + "\t\t" + lab_cost);
+
+ }
+
+}
+
+
+
+class fecility 
+
+{
+
+
+
+ String fec_name;
+
+ void add_feci()
+
+ {
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("facility:-");
+  fec_name = input.nextLine();
+
+
+ }
+
+ void show_feci()
+
+ {
+
+  System.out.println(fec_name);
+
+ }
+
+}
+
+
+
+public class hosp
+
+{
+
+ public static void main(String args[])
+
+ {
+
+  String months[] = {
+
+   "Jan",
+   "Feb",
+   "Mar",
+   "Apr",
+
+   "May",
+   "Jun",
+   "Jul",
+   "Aug",
+
+   "Sep",
+   "Oct",
+   "Nov",
+   "Dec"
+  };
+
+  Calendar calendar = Calendar.getInstance();
+
+  System.out.println("--------------------------------------------------------------------------------");
+
+  int count1 = 4, count2 =4, count3 = 4, count4 = 4, count5 = 4, count6 = 6;
+
+
+  System.out.println("--------------------------------------------------------------------------------");
+
+  System.out.println("                             HOSPITAL MANAGEMENT SYSTEM   ");
+
+  System.out.println("--------------------------------------------------------------------------------");
+  System.out.print("Date: " + months[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.DATE) + " " + calendar.get(Calendar.YEAR));
+
+  System.out.println("\t\t\t\t\t\tTime: " + calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND));
+
+
+  doctor d[] = new doctor[25];
+
+  patient p[] = new patient[100];
+  patient p1=new patient();
+
+  lab l[] = new lab[20];
+
+  fecility f[] = new fecility[20];
+
+  medical m[] = new medical[100];
+
+  staff s[] = new staff[100];
+
+  int i;
+
+  for (i = 0; i < 25; i++)
+
+	   d[i] = new doctor();
+
+	  for (i = 0; i < 100; i++)
+
+	   p[i] = new patient();
+
+	  for (i = 0; i < 20; i++)
+
+	   l[i] = new lab();
+
+	  for (i = 0; i < 20; i++)
+
+	   f[i] = new fecility();
+
+	  for (i = 0; i < 100; i++)
+
+	   m[i] = new medical();
+
+	  for (i = 0; i < 100; i++)
+
+	   s[i] = new staff();
+
+ d[0].did = "21";
+  d[0].dname = "Dr.ADNAN";
+  d[0].specilist = "cardio";
+  d[0].appoint = "10-4";
+  d[0].doc_qual = "Mbbs,Md";
+  d[0].droom = 17;
+
+  d[1].did = "32";
+  d[1].dname = "Dr.Akheel";
+  d[1].specilist = "ENT";
+  d[1].appoint = "10-4";
+  d[1].doc_qual = "mbbs,md";
+  d[1].droom = 45;
+
+  d[2].did = "17";
+  d[2].dname = "Dr.CHARITH";
+  d[2].specilist = "pediatric";
+  d[2].appoint = "10-4";
+  d[2].doc_qual = "bdm";
+  d[2].droom = 8;
+
+  d[3].did = "33";
+  d[3].dname = "Dr.AMAR";
+  d[3].specilist = "DENTIST";
+  d[3].appoint = "10-4";
+  d[3].doc_qual = "mbbs,ms";
+  d[3].droom = 40;
+
+
+  p[0].pid = "12";
+  p[0].pname = "ABHI";
+  p[0].disease = "Cancer";
+  p[0].sex = "male";
+  p[0].admit_status = "yes";
+  p[0].age = 30;
+
+  p[1].pid = "13";
+  p[1].pname = "deep";
+  p[1].disease = "cold";
+  p[1].sex = "male";
+  p[1].admit_status = "no";
+  p[1].age = 23;
+
+  p[2].pid = "14";
+  p[2].pname = "chand2";
+  p[2].disease = "malaria";
+  p[2].sex = "male";
+  p[2].admit_status = "yes";
+  p[2].age = 45;
+
+  p[3].pid = "15";
+  p[3].pname = "ravi";
+  p[3].disease = "diabeties";
+  p[3].sex = "male";
+  p[3].admit_status = "no";
+  p[3].age = 25;
+
+
+  m[0].med_name = "corex";
+  m[0].med_comp = "cino pvt";
+  m[0].exp_date = "9-5-16";
+  m[0].med_cost = 55;
+  m[0].count = 8;
+
+  m[1].med_name = "nytra";
+  m[1].med_comp = "ace pvt";
+  m[1].exp_date = "4-4-15";
+  m[1].med_cost = 500;
+  m[1].count = 5;
+
+  m[2].med_name = "Dolo";
+  m[2].med_comp = "cipla";
+  m[2].exp_date = "12-7-17";
+  m[2].med_cost = 50;
+  m[2].count = 56;
+
+  m[3].med_name = "saaz";
+  m[3].med_comp = "raj pvt";
+  m[3].exp_date = "12-4-12";
+  m[3].med_cost = 1100;
+  m[3].count = 100;
+
+
+  l[0].fecility = "x-ray     ";
+  l[0].lab_cost = 800;
+
+  l[1].fecility = "ct scan   ";
+  l[1].lab_cost = 1200;
+
+  l[2].fecility = "mri scan   ";
+  l[2].lab_cost = 15000;
+
+  l[3].fecility = "blood bank";
+  l[3].lab_cost = 50;
+
+
+  f[0].fec_name = "ambulane";
+
+  f[1].fec_name = "admit fec";
+
+  f[2].fec_name = "canteen";
+
+  f[3].fec_name = "free camp";
+
+
+  s[0].sid = "22";
+  s[0].sname = "ravi";
+  s[0].desg = "worker";
+  s[0].sex = "male";
+  s[0].salary = 5000;
+
+  s[1].sid = "23";
+  s[1].sname = "komal";
+  s[1].desg = "nurse";
+  s[1].sex = "female";
+  s[1].salary = 2000;
+
+  s[2].sid = "24";
+  s[2].sname = "raju";
+  s[2].desg = "worker";
+  s[2].sex = "male";
+  s[2].salary = 5000;
+
+  s[3].sid = "25";
+  s[3].sname = "rani";
+  s[3].desg = "nurse";
+  s[3].sex = "female";
+  s[3].salary = 20000;
+   
+  s[4].sid="55";
+	s[4].sname ="mahesh";
+	s[4].desg="security";
+	s[4].sex="male";
+	s[4].salary=15000;
+
+	 s[5].sid="55";
+		s[5].sname ="vivek";
+		s[5].desg="security";
+		s[5].sex="male";
+		s[5].salary=17000;
+
+  Scanner input = new Scanner(System.in);
+
+
+  int choice, j, c1, status = 1, s1 = 1, s2 = 1, s3 = 1, s4 = 1, s5 = 1, s6 = 1;
+
+  while (status == 1)
+
+  {
+
+   System.out.println("\n                                    MAIN MENU");
+
+   System.out.println("--------------------------------------------------------------------------------");
+
+   System.out.println("1.DOCTOR  2. PATIENT  3.MEDICINES  4.LAB  5. FACILITY  6.STAFF 7.EXIT");
+
+   System.out.println("--------------------------------------------------------------------------------");
+
+
+   choice = input.nextInt();
+
+   switch (choice)
+
+   {
+
+    case 1:
+
+     {
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      System.out.println("                        DOCTOR SECTION  ");
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      s1 = 1;
+
+      while (s1 == 1)
+
+      {
+
+       System.out.println("1.new entry\n2.doctor list");
+
+       c1 = input.nextInt();
+
+       switch (c1)
+
+       {
+
+        case 1:
+
+         {
+
+          d[count1].new_doctor();
+          count1++;
+
+          break;
+
+         }
+
+        case 2:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("id \t name\t        specilist \t timing \t qualification \t room no");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count1; j++)
+
+          {
+
+           d[j].doctor_info();
+
+          }
+
+          break;
+
+         }
+
+       }
+
+
+
+       System.out.println("Enter 1:Return to same section \n 2:GO to main menu ");
+
+       s1 = input.nextInt();
+
+      }
+
+      break;
+
+     }
+
+    case 2:
+
+     {
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      System.out.println("                       PATIENT SECTION  ");
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      s2 = 1;
+
+      while (s2 == 1)
+
+      {
+
+       System.out.println("1.new entry\n2.patient list\n3.Generate Bill");
+
+       c1 = input.nextInt();
+
+       switch (c1)
+
+       {
+
+        case 1:
+         {
+          p[count2].new_patient();
+          count2++;
+          break;
+         }
+
+        case 2:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("id \t name \t disease \t sex \t admit_status \t age");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count2; j++) {
+           p[j].patient_info();
+          }
+          break;
+         }
+        case 3:p1.genbill();
+        		break;
+        		
+
+       }
+
+       System.out.println("Enter 1:Return to same section \n 2:GO to main menu ");
+
+       s2 = input.nextInt();
+
+      }
+
+      break;
+
+     }
+
+    case 3:
+
+     {
+
+      s3 = 1;
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      System.out.println("                        MEDICINE SECTION   ");
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      while (s3 == 1)
+
+      {
+
+       System.out.println("1.new entry\n2.medicine list");
+
+       c1 = input.nextInt();
+
+       switch (c1)
+
+       {
+
+        case 1:
+         {
+          m[count3].new_medi();
+          count3++;
+          break;
+         }
+
+        case 2:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("name \t company \t expiry date \t cost");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count3; j++) {
+           m[j].find_medi();
+          }
+          break;
+         }
+
+       }
+
+       System.out.println("Enter 1:Return to same section \n 2:GO to main menu ");
+
+       s3 = input.nextInt();
+
+      }
+
+      break;
+
+     }
+
+    case 4:
+
+     {
+
+      s4 = 1;
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      System.out.println("                      LABORATRY SECTION  ");
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      while (s4 == 1)
+
+      {
+
+       System.out.println("1.new entry \n2.lab list");
+
+       c1 = input.nextInt();
+
+       switch (c1)
+
+       {
+
+        case 1:
+         {
+          l[count4].new_feci();count4++;
+          break;
+         }
+
+        case 2:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("facility\t\tcost");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count4; j++) {
+           l[j].feci_list();
+
+          }
+
+          break;
+
+         }
+
+       }
+
+       System.out.println("Enter 1:Return to same section \n 2:GO to main menu ");
+
+       s4 = input.nextInt();
+
+      }
+
+      break;
+
+     }
+
+    case 5:
+
+     {
+
+      s5 = 1;
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      System.out.println("            HOSPILITY FACILITY SECTION  ");
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      while (s5 == 1)
+
+      {
+
+       System.out.println("1.new entry\n2.facility list");
+
+       c1 = input.nextInt();
+
+       switch (c1)
+
+       {
+
+        case 1:
+         {
+          f[count5].add_feci();count5++;
+          break;
+         }
+
+        case 2:
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("hospility facility are:-");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count5; j++) {
+           f[j].show_feci();
+          }
+          break;
+         }
+
+       }
+
+       System.out.println("Enter 1:Return to same section \n 2:GO to main menu ");
+
+       s5 = input.nextInt();
+
+      }
+
+      break;
+
+     }
+
+    case 6:
+
+     {
+
+      s6 = 1;
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      System.out.println("                         STAFF SECTION  ");
+
+      System.out.println("--------------------------------------------------------------------------------");
+
+      while (s6 == 1)
+
+      {
+
+
+       String a = "nurse", b = "worker", c = "security";
+
+       System.out.println("1.new entry\n2.nurse list\n3.worker list \n4.security list");
+
+       c1 = input.nextInt();
+
+       switch (c1)
+
+       {
+
+        case 1:
+         {
+          s[count6].new_staff();count6++;
+          break;
+         }
+
+        case 2:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+
+          System.out.println("id \t name \t sex \t salary");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count6; j++)
+
+          {
+
+           if (a.equals(s[j].desg))
+
+            s[j].staff_info();
+
+          }
+
+          break;
+
+         }
+
+        case 3:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("id \t name \t sex \t salary");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count6; j++)
+
+          {
+
+           if (b.equals(s[j].desg))
+
+            s[j].staff_info();
+
+          }
+
+          break;
+
+         }
+
+        case 4:
+
+         {
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          System.out.println("id \t name \t sex \t salary");
+
+          System.out.println("--------------------------------------------------------------------------------");
+
+          for (j = 0; j < count6; j++)
+
+          {
+
+           if (c.equals(s[j].desg))
+
+            s[j].staff_info();
+
+          }
+
+          break;
+
+         }
+
+       }
+
+       System.out.println("Enter 1:Return to same section \n 2:GO to main menu ");
+
+       s6 = input.nextInt();
+
+      }
+
+      break;
+
+     }
+
+    case 7:
+
+     {
+
+      break;
+
+     }
+
+    default:
+
+     {
+
+      System.out.println("Wrong choice Entered \n Please enter a valid one ");
+      break;
+
+     }
+
+   }
+
+   System.out.println("Enter 1:Return to main menu \n 2:Terminate  ");
+
+   status = input.nextInt();
+
+  }
+
+ }
+
+}
+
+
+
